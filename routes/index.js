fdc499fd153e74695a42a7fb90be6ee00d54c311
vitalyaotletaesh <xxxamyakov@mail.ru>
2023-06-05 18:25:18 +0300

    try get ssl

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.use('/auth', authRouter)
 router.use('/test', testRouter)
 router.use('/question', questionRouter)
 router.use('/category', categoryRouter)
+router.use('/.well-known/pki-validation/BBDD04DB91295182F9ED51277894D200.txt', (req, res) => {
+    res.sendFile('../.well-known/pki-validation/BBDD04DB91295182F9ED51277894D200.txt')
+})
 
 export default router
